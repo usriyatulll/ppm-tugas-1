9dea0fbd4a4ec572f75ad0ebe1962ff2bc128e80
usriyatulll <102362593+usriyatulll@users.noreply.github.com>
2023-09-11 20:05:26 +0700

    ppm-tugas1 done

--- main.dart
+++ main.dart
@@ -6,10 +6,12 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    print(
+        "Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya ${DateTime.now().year - tahunLahir} tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021015", "Usriyatul Khamimah", 2003);
+  mhs.perkenalan();
 }
